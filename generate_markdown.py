3654d8ecb7db02009914fb258207d871a4aea782
Jan <110680154+Jan2903@users.noreply.github.com>
2025-01-13 21:20:49 +0100

    fix: wrong variable name

--- generate_markdown.py
+++ generate_markdown.py
@@ -88,8 +88,8 @@ def generate_site():
                         "jp": "ja",
                         "kr": "ko"
                  }
-                # Adjust languageCode based on the mapping
-                languageCode = mapping.get(languageCode, languageCode)
+                # Adjust language_code based on the mapping
+                language_code = mapping.get(languageCode, languageCode)
 
                 language_output_dir = os.path.join(OUTPUT_DIR, language_code)
                 # Ensure the language-specific output directory exists
